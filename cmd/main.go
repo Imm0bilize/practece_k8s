60e5a452b426daea06638e8d91fd5b3706f97e6c
init commit <vst515@yandex.ru>
2022-02-13 13:16:25 +0300

    Init commit

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("|  %s  |  %s  |  %s", r.Host, r.Method, r.RequestURI)
+	_, err := fmt.Fprintf(w, "Message: %v", r.URL.Path[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module practice_k8s
+
+go 1.17
